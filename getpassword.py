63930a5d0fb2ec37bea95a8bf395a69388033d33
Vkay23 <57458523+Vkay23@users.noreply.github.com>
2026-04-21 00:58:56 +0530

    Create getpassword.py

---
+++ getpassword.py
@@ -0,0 +1,36 @@
+import json
+import os
+import random
+import string 
+from getpass import getpass
+
+FILE = "passwords.json"
+
+def load_data():
+    if not os.path.exists(FILE):
+         return {}
+    try: 
+       with open(FILE) as f:
+          return json.load(f)
+
+    except: 
+       return {} 
+
+   def save_data(data):
+       with open(FILE, "w") as f:
+          json.dump(data,f,indent=4)
+
+  def generate_password(length=12):
+       chars= string.ascii_letters + \
+           string.digits + "!@#$%^&*"
+       return ''.join(random.choice(chars)
+           for _ in range(length))
+
+   def add_password():
+       data = load_data()
+       site = input("Website/App name: ").strip()
+       username = input("Username/Email: ").strip() 
+       choice = input("1. Enter password " \ "\n2. Generate strong password: ")
+
+       if choice == "2":
+            pwd = generate_password
